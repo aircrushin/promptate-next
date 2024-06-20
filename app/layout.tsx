35ed22a9e3299427745c598842fc23a986c9cf6b
UltraV <1216564567@qq.com>
2024-06-20 15:38:19 +0800

    update: fix null prompt bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Abyssinica_SIL } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const inter = Abyssinica_SIL({ weight: ['400'], subsets: ['latin']});
+const inter = Abyssinica_SIL({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Promptate",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
 import FAQ from "@/components/FAQ";
 import { Textarea } from "@/components/ui/textarea";
 import axios from "axios";
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter } from "react-simple-typewriter";
+import { toast } from "react-toastify";
 
 export default function Hero() {
   const [text, setText] = useState("");
@@ -23,13 +23,25 @@ export default function Hero() {
   const data: DeepseekRequest = {
     model: "deepseek-chat",
     messages: [
-      { role: "system", content: "你是一个提示词生成专家，名为 promptate，擅长为用户优化和生成提示词" },
-      { role: "user", content: `我正在尝试从以下提示词中获得 GPT-4 的良好结果：“${text}”。你能否写出更优化、能够产生更好结果的提示词？请直接回复您优化的提示词给我。` },
+      {
+        role: "system",
+        content:
+          "你是一个提示词生成专家，名为 promptate，擅长为用户优化和生成提示词",
+      },
+      {
+        role: "user",
+        content: `我正在尝试从以下提示词中获得 GPT-4 的良好结果：“${text}”。你能否写出更优化、能够产生更好结果的提示词？请直接回复您优化的提示词给我。`,
+      },
     ],
     stream: false,
   };
 
   const handleGenerate = () => {
+    if (!text || text.trim() === "") {
+      toast.error("you must enter a prompt to start generate");
+      return;
+    }
+
     axios
       .post("https://api.deepseek.com/chat/completions", data, {
         headers: {
@@ -57,24 +69,23 @@ export default function Hero() {
           </p>
           <div className="flex justify-center space-x-4 h-[30vh]">
             <Textarea
-              placeholder="Enter text here"
+              className="text-gray-200 bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500"
+              placeholder=""
               value={text}
               onChange={(e) => setText(e.target.value)}
             />
             <button
               title="Generate prompt"
               onClick={handleGenerate}
-              className="justify-center items-center my-auto px-4 py-2 h-10 text-sm text-blue-500 border border-gray-200 rounded-md shadow-sm bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:scale-105 transition-transform duration-300"
+              className="bg-slate-200 justify-center items-center my-auto px-4 py-2 h-10 text-sm text-blue-500 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 hover:scale-105 transition-transform duration-300"
             >
               generate
             </button>
-            <div className="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
+            <div className="text-gray-200 text-start flex min-h-[80px] w-full rounded-md border border-input bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
               {prompt && (
                 <Typewriter
                   words={[prompt]}
                   loop={1}
-                  cursor
-                  cursorStyle="_"
                   typeSpeed={50}
                   deleteSpeed={50}
                   delaySpeed={1000}
